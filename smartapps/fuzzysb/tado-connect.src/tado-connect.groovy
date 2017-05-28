2ea9886d89742a5070abb1341e27ded753b52a1f
Stuart <stuart@buchanan-uk.info>
2017-05-28 12:06:04 +0100

    removed erronous jsonbody statement from the coolCommand function

--- smartapps/fuzzysb/tado-connect.src/tado-connect.groovy
+++ smartapps/fuzzysb/tado-connect.src/tado-connect.groovy
@@ -12,6 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ * 26/05/2017 v2.3 removed erronous jsonbody statements in the coolCommand Function.
  * 26/05/2017 v2.2 Corrected bug with parseCapability function as this was returning the map instead of the value, this would account for lots of strange behaviour.
  * 25/05/2017 v2.1 Added support for Air Condiioners which have a mandatory swing field for all Commands, corrected prevois bugs in v2.0, thanks again to @Jnick
  * 20/05/2017 v2.0 Added support for Air Condiioners which have a mandatory swing field in the heating & cool Commands, thanks again to @Jnick
@@ -1712,14 +1713,6 @@ def coolCommand(childDevice){
     	else if (state.tempunit == "F"){
     		jsonbody = new groovy.json.JsonOutput().toJson([setting:[fanSpeed:supportedfanspeed, mode:"COOL", power:"ON", temperature:[fahrenheit:initialsetpointtemp], type:"AIR_CONDITIONING"], termination:[type:terminationmode]])
     	}
-    }
-    
-    
-    if (state.tempunit == "C") {
-    jsonbody = new groovy.json.JsonOutput().toJson([setting:[fanSpeed:supportedfanspeed, mode:"COOL", power:"ON", temperature:[celsius:initialsetpointtemp], type:"AIR_CONDITIONING"], termination:[type:terminationmode]])
-    }
-    else if (state.tempunit == "F"){
-    jsonbody = new groovy.json.JsonOutput().toJson([setting:[fanSpeed:supportedfanspeed, mode:"COOL", power:"ON", temperature:[fahrenheit:initialsetpointtemp], type:"AIR_CONDITIONING"], termination:[type:terminationmode]])
     }
     sendCommand("temperature",childDevice,[deviceId,jsonbody])
 }
